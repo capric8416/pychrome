d59744c00c9df55c61a3933abfd459e9f32e28d2
fate0 <fate0@fatezero.org>
2017-08-01 23:04:37 +0800

    wip

--- pychrome/browser.py
+++ pychrome/browser.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import unicode_literals, with_statement
+from __future__ import unicode_literals
 
 import logging
 import requests
